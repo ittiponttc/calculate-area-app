446e43d8a418fcfcd90f970f31595624c7434a7a
ittiponttc <ittipon.kmutnb@gmail.com>
2026-01-11 21:32:08 +0700

    Update rigid_pavement_aashto.py

--- rigid_pavement_aashto.py
+++ rigid_pavement_aashto.py
@@ -1,6 +1,3 @@
-Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> """
 ... โปรแกรมคำนวณความหนาโครงสร้างชั้นทางคอนกรีต (Rigid Pavement)
 ... ตามวิธี AASHTO 1993
 ... พัฒนาสำหรับการเรียนการสอนวิศวกรรมโยธา
